870b45353fc4f97e3352ce4efc2f6d3091e42df8
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2022-01-21 19:46:19 -0500

    update for other changes in SDV 1.6

--- LookupAnything/Framework/Lookups/Tiles/TileSubject.cs
+++ LookupAnything/Framework/Lookups/Tiles/TileSubject.cs
@@ -66,7 +66,7 @@ namespace Pathoschild.Stardew.LookupAnything.Framework.Lookups.Tiles
                     yield return new GenericField(I18n.Tile_TileIndex(layerName: layerName), this.Stringify(tile.TileIndex));
                     yield return new GenericField(I18n.Tile_Tilesheet(layerName: layerName), tile.TileSheet.ImageSource.Replace("\\", ": ").Replace("/", ": "));
                     yield return new GenericField(I18n.Tile_BlendMode(layerName: layerName), this.Stringify(tile.BlendMode));
-                    foreach (KeyValuePair<string, PropertyValue> property in tile.TileIndexProperties)
+                    foreach (KeyValuePair<string, PropertyValue> property in tile.TileIndexProperties.GetCollection())
                         yield return new GenericField(I18n.Tile_IndexProperty(layerName: layerName, propertyName: property.Key), property.Value);
                     foreach (KeyValuePair<string, PropertyValue> property in tile.Properties)
                         yield return new GenericField(I18n.Tile_TileProperty(layerName: layerName, propertyName: property.Key), property.Value);
